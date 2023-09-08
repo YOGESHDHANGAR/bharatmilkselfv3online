8fc175d058c31c3a209db838ffc3eeacc0094a50
YOGESHDHANGAR <vijay45155@gmail.com>
2023-09-08 11:45:29 +0530

    trying to use create pool

--- databases/database.js
+++ databases/database.js
@@ -1,18 +1,18 @@
+const mysql = require("mysql");
+
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config({ path: "config/config.env" });
 }
-const mysql = require("mysql");
 
-const con = mysql.createConnection({
+const con = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   timezone: process.env.DB_TIMEZONE,
-  max_allowed_packet: 1024 * 1024 * 10,
 });
 
-con.connect((err) => {
+con.getConnection((err) => {
   if (err) {
     console.log(err.message);
   } else {
